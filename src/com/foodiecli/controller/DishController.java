99644d2d4f1c13431be5bd6e449e1b71da74b6d8
Raju <isaacraj890@gmail.com>
2024-07-12 17:27:24 +0530

    feat: Added DishController class
    - Implemented DishController class to manage Dishes-related operations through the DishServiceImp service.

---
+++ src/com/foodiecli/controller/DishController.java
@@ -0,0 +1,36 @@
+package com.foodiecli.controller;
+
+import com.foodiecli.exceptions.DishAlreadyExistsException;
+import com.foodiecli.exceptions.DishNotFoundException;
+import com.foodiecli.model.Dish;
+import com.foodiecli.service.DishServiceImp;
+
+import java.util.List;
+
+public class DishController {
+    private final DishServiceImp dishService;
+
+    public DishController(DishServiceImp dishService) {
+        this.dishService = dishService;
+    }
+
+    public List<Dish> getDishesList(){
+        return this.dishService.getDishesList();
+    }
+
+    public Dish save(Dish dish) throws DishAlreadyExistsException{
+        return this.dishService.save(dish);
+    }
+
+    public Dish getDishById(String id) throws DishNotFoundException{
+        return this.dishService.getDishById(id);
+    }
+
+    public Dish updateDish(Dish dish) throws DishNotFoundException{
+        return this.dishService.updateDish(dish);
+    }
+
+    public void delete(String id) throws DishNotFoundException{
+        this.dishService.delete(id);
+    }
+}
